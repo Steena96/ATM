29de9c82d8952ccf3206569379e919913acdcc68
Steena Hyacinth <steenahyacinth@Admins-MacBook-Pro.local>
2023-09-06 22:36:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './global.css'
+
+function App() {
+  return (
+    <>
+      <div className='bg-red-500 text-white'>
+       Helloo ATM Project!
+      </div>
+    </>
+  )
+}
+
+export default App
